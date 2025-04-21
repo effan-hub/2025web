8f352ac979ea5b87f922e2dfd1565105befee4ba
effan-hub <effan.android@gmail.com>
2025-04-21 17:11:04 +0800

    edit

---
+++ editaksimahasiswa.php
@@ -0,0 +1,15 @@
+<?php
+include "koneksi.php";
+
+$nim = $_POST["nim"];
+$nama = $_POST["nama"];
+$tanggalLahir = $_POST["tanggalLahir"];
+$telp = $_POST["telp"];
+$email = $_POST["email"];
+$id = $_POST["id_prodi"];
+
+$query = "UPDATE mahasiswa SET nama = '$nama', tanggalLahir = '$tanggalLahir', telp = '$telp', email = '$email', id_prodi = '$id' WHERE nim = '$nim'";
+
+mysqli_query($conn, $query);
+
+header("location:index.php");
